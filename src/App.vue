3937941cdaf00c3f9cb8101c523e3ae475fc7acd
muhe <muhe@kerrylan.com>
2023-04-25 13:50:15 +0800

    修改图片路径

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <el-carousel :interval="4000" type="card" indicator-position="outside" :height="bannerHeight + 'px'" width="100%">
+    <el-carousel :interval="4000" type="card" indicator-position="outside" :height="screenHeight + 'px'" width="100%">
         <el-carousel-item v-for="item in imgList" :key="item">
             <img :src="item" alt="img" style="max-width:100%;max-height:100%"/>
         </el-carousel-item>
@@ -11,36 +11,36 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                imgList: [
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                    '/home/vue/src/assets/img/lixue1.jpg',
-                ],
-                bannerHeight: 0
-            }
-        },
-        mounted () {
-            this.setSize();
-            const that = this;
-            window.addEventListener('resize', function() {
-                that.screenWidth = $(window).width();
-                that.setSize();
-            }, false);
-        },
-        methods: {
-            setSize: function () {
-                this.bannerHeight = 740 / 2560 * this.screenWidth
-                if(this.bannerHeight > 740) this.bannerHeight = 740
-                if(this.bannerHeight < 360) this.bannerHeight = 360
-            }
+export default {
+    data() {
+        return {
+            imgList: [
+                '/img/lixue1.jpg',
+                '/img/lixue1.jpg',
+                '/img/lixue1.jpg',
+                '/img/lixue1.jpg',
+                '/img/lixue1.jpg',
+                '/img/lixue1.jpg',
+            ],
+            screenHeight: 0
+        }
+    },
+    created() {
+        this.screenHeight = this.autoHeight()
+    },
+    mounted() {
+        // window.onresize = () => {
+        //     return (() => {
+        //         this.screenHeight = document.documentElement.clientHeight + 'px'
+        //     })
+        // }
+    },
+    methods: {
+        autoHeight(){
+            return document.documentElement.clientHeight + 'px'
         }
     }
+}
 </script>
 
 
