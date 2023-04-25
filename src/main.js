78de770db21db9bc0a8cd8c0afa14c846dfdb9ce
muhe <muhe@kerrylan.com>
2023-04-25 09:41:54 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import elementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import './assets/main.css'
+
+
+createApp(App).use(elementPlus).mount('#app')
