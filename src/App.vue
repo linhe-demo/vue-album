243758a5ffc21d247527fb4ec543b434c5236e61
muhe <muhe@kerrylan.com>
2023-09-14 19:04:12 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" style="height: 100%;">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
@@ -9,7 +9,38 @@ import {useRouter} from "vue-router";
 
 export default {
   name: 'App',
-  mounted () {
+  // 禁止web端屏幕缩放
+  created() {
+    window.addEventListener("mousewheel", function (event) {
+      if (event.ctrlKey === true || event.metaKey) {
+        event.preventDefault();
+      }
+    }, {passive: false});
+    /*禁止ios缩放，双击和双指*/
+    window.onload = function() {
+      document.addEventListener("touchstart", function(event) {
+        if (event.touches.length > 1) {
+          event.preventDefault();
+        }
+      });
+      var lastTouchEnd = 0;
+      document.addEventListener(
+          "touchend",
+          function(event) {
+            var now = new Date().getTime();
+            if (now - lastTouchEnd <= 300) {
+              event.preventDefault();
+            }
+            lastTouchEnd = now;
+          },
+          false
+      );
+      document.addEventListener("gesturestart", function(event) {
+        event.preventDefault();
+      });
+    };
+  },
+  mounted() {
     const router = useRouter()
     // 根据不同路由跳转不同页面
     if (!this._isMobile()) {
@@ -27,7 +58,7 @@ export default {
   },
   methods: {
     // 判断是否是手机端，如果是，返回true
-    _isMobile () {
+    _isMobile() {
       return navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
     }
   }
