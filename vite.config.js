83145572fa9390a3b4bc9a108b296b677bbe8438
muhe <muhe@kerrylan.com>
2023-09-14 16:21:37 +0800

    修改样式

--- vite.config.js
+++ vite.config.js
@@ -30,10 +30,5 @@ export default defineConfig({
             'BASE_URL': 'http://life-moment.top:8806'
             // 'REQUEST_URL': 'http://127.0.0.1:8806'
         }
-    },
-    build: {
-        rollupOptions: {
-            external: ['vuex']
-        }
     }
 })
