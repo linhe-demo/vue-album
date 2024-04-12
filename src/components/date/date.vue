7d01f87422badd036ac12ee29a60dc84bdd05e37
muhe <muhe@kerrylan.com>
2024-04-12 18:44:41 +0800

    优化

--- src/components/date/date.vue
+++ src/components/date/date.vue
@@ -23,15 +23,25 @@
         </div>
       </div>
       <div class="windmill">
-        <img class="rotate-image" src="http://www.life-moment.top/images/static/fengche.png">
+        <img v-if="imageLoaded" class="rotate-image" src="http://www.life-moment.top/images/static/fengche.png" alt="">
       </div>
     </div>
   </div>
   <div class="date-box" v-loading.fullscreen="loadDate" :element-loading-text="loading">
-    <div class="card-list">
-      <el-card class="card" v-for="(item, index) in dateList" :key="index" shadow="always" @click="showTimeLine(item)">
-        {{ item }}
-      </el-card>
+    <div class="user-img" v-if="imgList">
+      <div></div>
+      <el-carousel :interval="3000" style="height: 100%;border-radius: 5px;" arrow="hover" >
+        <el-carousel-item v-for="(value, index) in imgList" :key="index">
+          <img :src="value.src" alt="" height="100%" width="100%" style="object-fit: cover">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="daily">
+      <div class="card-list">
+        <el-card class="card" v-for="(item, index) in dateList" :key="index" shadow="always" @click="showTimeLine(item)">
+          {{ item }}
+        </el-card>
+      </div>
     </div>
   </div>
   <div class="foot">
@@ -69,7 +79,9 @@ export default {
       },
       loadDate: false,
       loading: "数据加载中，请稍后！",
-      dateInfo: []
+      dateInfo: [],
+      imgList: false,
+      imageLoaded: false
     }
   },
   mounted() {
@@ -78,6 +90,7 @@ export default {
     this.customer = store.state.user.nickname
     this.token = store.state.user.token
     this.route = useRouter()
+    this.getImgList()
     this.getConfig()
     this.getDateInfo()
   },
@@ -92,6 +105,25 @@ export default {
       }).then(response => {
         if (response.data.code === 200) {
           this.dateList = response.data.data
+        } else {
+          alert(response.data.message);
+          this.route.replace({path: '/'});
+        }
+        this.loadAlbum = false
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    getImgList() {
+      this.loadAlbum = true
+      let store = useStore()
+      axios.post(process.env.BASE_URL + '/api/v1/image/list', {}, {
+        headers: {
+          'Authorization': store.state.user.token
+        }
+      }).then(response => {
+        if (response.data.code === 200) {
+          this.imgList = response.data.data
         } else {
           alert(response.data.message);
           this.route.replace({path: '/'});
@@ -137,7 +169,11 @@ export default {
       this.route.replace({
         path: '/baby'
       })
-    }
+    },
+    handleLoad() {
+      this.imageLoaded = true
+      console.log("111")
+    },
   }
 }
 </script>
@@ -148,8 +184,8 @@ export default {
   height: 20%;
   vertical-align: middle;
   border-radius: 5px;
-  margin: 1px;
-  background: transparent url("http://www.life-moment.top/images/static/flower.jpg");
+  padding: 1px;
+  background: transparent url("http://www.life-moment.top/images/static/time.jpg");
   background-size: cover;
 }
 
@@ -171,9 +207,13 @@ export default {
 .date-box {
   width: 100%;
   height: 68%;
-  overflow-y: auto;
   background: transparent url("http://www.life-moment.top/images/static/bk4.jpg") center;
   background-size: cover;
+}
+
+.daily {
+  height: 70%;
+  overflow-y: auto;
 }
 
 .card-list {
@@ -223,6 +263,13 @@ export default {
   line-height: 20px;
 }
 
+.user-img {
+  padding-top: 10px;
+  padding-right: 10px;
+  padding-left: 10px;
+  height: 30%;
+}
+
 .info-line {
   display: flex;
   padding-left: 10px;
@@ -237,7 +284,7 @@ export default {
 .windmill{
   position: absolute;
   right: 2%;
-  top: 13%;
+  top: 1%;
   width: 40px;
   height: 40px;
   z-index: 100;
@@ -262,5 +309,34 @@ export default {
     transform: rotate(360deg);
   }
 }
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.1;
+  line-height: 100px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+::v-deep(.el-carousel__indicators) {
+  display: none;
+}
+
+::v-deep(.el-carousel__container) {
+  height: 100%;
+}
+
+::v-deep(.is-always-shadow) {
+  opacity: 0.5;
+}
+
+
 </style>
 
